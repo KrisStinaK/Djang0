4aad148cca3c236db0250709e76db4e43aff6a28
KrisStinaK <112583110+KrisStinaK@users.noreply.github.com>
2024-12-16 20:41:07 +0300

    Optimization of sql queries

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.static import static
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from coolsite import settings
@@ -6,8 +7,7 @@ from coolsite import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sportsman.urls')),
-] \
-              # + debug_toolbar_urls()
+] + debug_toolbar_urls()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
